c1a4f4ba4cd98a4fa9a7fac5dcf4e934ba43e4ff
chen <1443977802@qq.com>
2023-11-29 20:23:13 +0800

    初始化DoDocs

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+
+const theme: Theme = {
+  ...DefaultTheme,
+  // enhanceApp({}){
+
+  // }
+}
+
+export default theme
---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { resolve } from 'node:path'
+import { createRequire } from 'node:module'
+import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import Icons from 'unplugin-icons/vite'
+
+// import { MarkdownTransform } from './.vitepress/plugins/markdownTransform'
+
+const require = createRequire(import.meta.url)
+
+export default defineConfig(async () => {
+  return <UserConfig>{
+    server: {
+      hmr: {
+        overlay: false,
+      },
+      fs: {
+        allow: [
+          resolve(__dirname, '..'),
+        ],
+      },
+    },
+    plugins: [
+      // // custom
+      // MarkdownTransform(),
+      // // plugins
+      // Components({
+      //   dirs: resolve(__dirname, '.vitepress/theme/components'),
+      //   include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      //   resolvers: [
+      //     IconsResolver({
+      //       componentPrefix: '',
+      //     }),
+      //   ],
+      //   dts: './.vitepress/components.d.ts',
+      //   transformer: 'vue3',
+      // }),
+      Icons({
+        compiler: 'vue3',
+        autoInstall: true,
+        defaultStyle: 'display: inline-block',
+      }),
+      UnoCSS(),
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          require('postcss-nested'),
+        ],
+      },
+    },
+  }
+})
